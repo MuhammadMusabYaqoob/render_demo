1a4cc8e93befe55e7469b678fc87ce755152ed65
MuhammadMusabYaqoob <mmusab.yaqoob@gmail.com>
2025-07-28 07:26:48 +0500

    update

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def predict():
         return render_template('error.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(debug=True)
